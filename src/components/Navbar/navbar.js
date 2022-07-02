4e769bfe420c68e19158c526d8fd676dd6ab7135
Luis Michael Goyone <luismichaelgoyone@gmail.com>
2022-07-02 19:35:06 +0800

    chore: updated navbar

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -82,18 +82,6 @@ const Navbar = ({ toggleSidebar }) => {
                   Projects
                 </NavLinks>
               </NavItem>
-              <NavItem>
-                <NavLinks
-                  to="contact"
-                  smooth={true}
-                  duration={500}
-                  spy={true}
-                  exact="true"
-                  offset={-80}
-                >
-                  Contact
-                </NavLinks>
-              </NavItem>
             </NavMenu>
           </NavbarContainer>
         </Nav>
